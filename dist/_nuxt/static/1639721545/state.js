0db495bc8a244dd1358ce9563b0a4835752266fe
Alexandr Elkin <53054644+YorKaen@users.noreply.github.com>
2021-12-17 09:20:07 +0300

    4

--- dist/_nuxt/static/1639721545/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fapp\u002F_nuxt\u002Fstatic\u002F1639721545",layout:"main",error:a,state:{global:{userId:""},local:{loggedIn:false,arrayStore:[]}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fapp\u002F",assetsPath:"\u002Fapp\u002F_nuxt\u002F",cdnURL:a}}}}(null));
